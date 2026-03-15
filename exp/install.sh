0061ae28eb52053a120aab8349a534adadaa7672
Rishabh Manoj <rishabh.manoj@google.com>
2026-03-15 17:44:49 +0000

    Add install.sh

---
+++ exp/install.sh
@@ -0,0 +1,7 @@
+pip install -e . && \
+pip install transformers accelerate && \
+pip install torch --index-url https://download.pytorch.org/whl/cpu && \
+pip install -U jax[tpu] && \
+pip install torchax && \
+pip install flax && \
+pip install ftfy imageio imageio-ffmpeg
