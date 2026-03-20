98fc1505c2758c01b0ce27d31f17c3e3bbe839c0
Rishabh Manoj <rishabhmanojnlc@gmail.com>
2026-03-21 02:43:25 +0530

    Pin jax[tpu] version to 0.9.1 in install script

--- exp/install.sh
+++ exp/install.sh
@@ -1,7 +1,7 @@
 pip install -e .. && \
 pip install transformers accelerate && \
 pip install torch --index-url https://download.pytorch.org/whl/cpu && \
-pip install -U jax[tpu] && \
+pip install -U jax[tpu]==0.9.1 && \
 pip install torchax && \
 pip install flax && \
 pip install ftfy imageio imageio-ffmpeg
